cdbd5df47e7338c6990465aa8f9190fbee6d22b3
barkov.ae <barkov.ae@talantiuspeh.ru>
2019-08-10 13:41:58 +0300

    homework #8 - ansible-1

--- terraform/stage/outputs.tf
+++ terraform/stage/outputs.tf
@@ -5,3 +5,7 @@ output "apps_external_ip" {
 output "db_external_ip" {
   value = "${module.db.db_external_ip}"
 }
+
+output "dynamic_inventory" {
+  value = "${template_file.dynamic_inventory.rendered}"
+}
