421a527bb907a10f5f90c1889224191069c7b113
barkov.ae <barkov.ae@talantiuspeh.ru>
2019-08-10 22:43:12 +0300

    travis

--- travis.sh
+++ travis.sh
@@ -6,13 +6,13 @@ DOCKER_IMAGE=express42/otus-homeworks
 echo "Run tests"
 
 # Prepare network & run container
-docker network create hw-test-net
+docker network create hw-infratest-net
 docker run -d -v $(pwd):/srv -v /var/run/docker.sock:/tmp/docker.sock \
     -e DOCKER_HOST=unix:///tmp/docker.sock --cap-add=NET_ADMIN --privileged \
-    --device /dev/net/tun --name hw-test --network hw-test-net $DOCKER_IMAGE
+    --device /dev/net/tun --name hw-infratest --network hw-infratest-net $DOCKER_IMAGE
 #docker exec -e USER=appuser -it hw-test bash
-docker exec -e USER=appuser hw-test sh -c "./infra-tests/ansible.sh"
-docker exec -e USER=appuser hw-test sh -c "./infra-tests/packer.sh"
-docker exec -e USER=appuser hw-test sh -c "./infra-tests/terraform.sh"
-docker container rm -f hw-test
-docker network rm hw-test-net
+docker exec -e USER=appuser hw-infratest sh -c "./infra-tests/ansible.sh"
+docker exec -e USER=appuser hw-infratest sh -c "./infra-tests/packer.sh"
+docker exec -e USER=appuser hw-infratest sh -c "./infra-tests/terraform.sh"
+docker container rm -f hw-infratest
+docker network rm hw-infratest-net
